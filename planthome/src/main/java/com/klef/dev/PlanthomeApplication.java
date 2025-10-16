9d23c70138deb69c74bae8a518636c319e1f0885
Hemanth <2300032944@kluniversity.in>
2025-10-16 11:11:44 +0530

    project

--- planthome/src/main/java/com/klef/dev/PlanthomeApplication.java
+++ planthome/src/main/java/com/klef/dev/PlanthomeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlanthomeApplication extends SpringBootServletInitializer{
+public class PlanthomeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanthomeApplication.class, args);
